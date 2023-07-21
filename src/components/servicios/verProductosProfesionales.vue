8a2edc130c389a36d88d78b0d9b2695d59eaa124
Nicolas Magnani <nmagnani@iupik.com>
2023-07-20 23:20:26 -0300

    dist

--- src/components/servicios/verProductosProfesionales.vue
+++ src/components/servicios/verProductosProfesionales.vue
@@ -122,7 +122,6 @@ export default {
         const response = await ServiciosService.get_ultimas_servicios_paginate(
           1
         );
-        console.log(response);
         if (response.data.error == false) {
           this.productos = response.data.data;
           this.totalRows = response.data.cantidad;
